42d65ee80d2d7d4d043a476c6811064bdc212b0e
Kino  Rose <kinoarose@gmail.com>
2018-06-06 19:42:23 -0400

    Add product and home page

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">Banner
+        <img src="" alt="responsive image" class="img-fluid"> 
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2>The Cure For Your Shopping Needs!</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <div v-bind:key="item.title" v-for="item in needs" class="col home-needs">
+            {{item.title}} 
+            <div class="home-description">
+              <p>{{item.description}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      needs:[
+        {title:'Quick!', description: 'Get what you need fast!'}, 
+        {title:'Quality!', description: 'Highest quality in the stare area!'}, 
+        {title:'Cheap!', description: 'Prices competitive with other marketers!'}
+      ]
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
-
+import Home from '@/components/Home'
+import About from '@/compontents/About'
+import Products from '@/components/Products'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
-      component: Main
+      name: 'Home',
+      component: Home
+    }, 
+    {
+      path: '/products',
+      name: 'Products',
+      component: null
+    }, 
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
